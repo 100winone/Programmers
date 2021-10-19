fab65f31bbd2354a9192fa0e87cc6f15bb1114da
winone <100winone@naver.com>
2021-10-19 23:53:06 +0900

    (2021.10.19) 로또의 최고 순위와 최저 순위

---
+++ Python_code/LV1/rank_lotto.py
@@ -0,0 +1,11 @@
+def solution(lottos, win_nums):
+    answer = [0, 0]
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    ans = 0
+    zero_cnt = lottos.count(0)
+    for i in win_nums:
+        if i in lottos:
+            ans += 1
+    answer[0] = rank[ans + zero_cnt]
+    answer[1] = rank[ans]
+    return answer
