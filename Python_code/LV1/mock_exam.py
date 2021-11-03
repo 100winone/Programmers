308fbff862cc2a989461d9d13da64bfe4dc494d6
winone <100winone@naver.com>
2021-11-03 22:30:32 +0900

    (2021.11.03) 모의고사

---
+++ Python_code/LV1/mock_exam.py
@@ -0,0 +1,19 @@
+def solution(answers):
+    answer = []
+    correct_arr = []
+    picked_way = [[1, 2, 3, 4, 5]
+                , [2, 1, 2, 3, 2, 4, 2, 5]
+                , [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+
+    for i in range(len(picked_way)):
+        cnt = 0
+        for j in range(len(answers)):
+            k = j % len(picked_way[i])
+            if answers[j] == picked_way[i][k]:
+                cnt += 1
+        correct_arr.append(cnt)
+    max_val = max(correct_arr)
+    for i in range(len(correct_arr)):
+        if correct_arr[i] == max_val:사
+            answer.append(i + 1)
+    return answer
