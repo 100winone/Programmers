2204af9d553be7db66c3ce7e748e3095da139281
winone <100winone@naver.com>
2021-11-18 21:28:26 +0900

    (2021.11.13) 타겟 넘버

--- Python_code/LV2/target_number.py
+++ Python_code/LV2/target_number.py
@@ -1,16 +1,18 @@
-def dfs(arr, tar, cnt, sum):
-    if cnt == len(arr):
-        if sum == tar:
-
-
-
+answer = 0
 def solution(numbers, target):
+    def dfs(cnt, sum):
+        global answer
+        if cnt == len(numbers):
+            if sum == target:
+                answer += 1
+            return
 
-    dfs(numbers, target, 0, 0)
+        dfs(cnt + 1, sum - numbers[cnt])
+        dfs(cnt + 1, sum + numbers[cnt])
+
+    dfs(0, 0)
     return answer
 
 
 print(solution([1, 1, 1, 1, 1], 3))
-
 # 종료조건과 재귀로 호출할 형태 구성
-#
