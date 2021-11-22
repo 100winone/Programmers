8ed80001067ad5b0b92266fdda4c3d5375a38715
winone <100winone@naver.com>
2021-11-22 21:11:28 +0900

    (2021.11.22) 단어 변환

---
+++ Python_code/LV3/switch_word.py
@@ -0,0 +1,40 @@
+answer = 101
+check = [0 for _ in range(51)]
+
+
+def diff(a_word, b_word):
+    dif_cnt = 0
+    for i in range(0, len(a_word)):
+        if a_word[i] != b_word[i]:
+            dif_cnt += 1
+    if dif_cnt == 1:
+        return dif_cnt
+    else:
+        return 0
+
+
+def dfs(begin, target, words, cnt):
+    global answer
+
+    for i in range(0, len(words)):
+        if diff(begin, words[i]) == 1:
+            if target == words[i]:
+                if answer > cnt + 1:
+                    answer = cnt + 1
+                    return
+            if check[i] == 0:
+                check[i] = 1
+                dfs(words[i], target, words, cnt + 1)
+                check[i] = 0
+
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    dfs(begin, target, words, 0)
+
+    return answer
+
+
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
