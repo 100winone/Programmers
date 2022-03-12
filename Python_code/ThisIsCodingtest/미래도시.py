37dfec1df06da2be0092fc2d8b1af4e3d2dfe3d8
winone <100winone@naver.com>
2022-03-13 01:03:30 +0900

    미래 도시 (2022.03.13)

---
+++ Python_code/ThisIsCodingtest/미래도시.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+INF = int(1e9)
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+answer = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+for t in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][t] + graph[t][b])
+
+answer = graph[1][k] + graph[k][x]
+
+if answer >= INF:
+    print("-1")
+else:
+    print(answer)
