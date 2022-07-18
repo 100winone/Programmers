a002a85358b2bc6028520af2244381250b704da2
winone <100winone@naver.com>
2022-07-18 20:35:00 +0900

    타겟 넘버 (2022.07.18)

--- Python_code/LV2/target_number.py
+++ Python_code/LV2/target_number.py
@@ -1,16 +1,32 @@
+# answer = 0
+# def solution(numbers, target):
+#     def dfs(cnt, sum):
+#         global answer
+#         if cnt == len(numbers):
+#             if sum == target:
+#                 answer += 1
+#             return
+#
+#         dfs(cnt + 1, sum - numbers[cnt])
+#         dfs(cnt + 1, sum + numbers[cnt])
+#
+#     dfs(0, 0)
+#     return answer
+
 answer = 0
+
+def dfs(idx, numbers, target, cur_sum):
+    global answer
+    if idx == len(numbers):
+        if cur_sum == target:
+            answer += 1
+        return
+    dfs(idx + 1, numbers, target, cur_sum + numbers[idx])
+    dfs(idx + 1, numbers, target, cur_sum - numbers[idx])
+
+
 def solution(numbers, target):
-    def dfs(cnt, sum):
-        global answer
-        if cnt == len(numbers):
-            if sum == target:
-                answer += 1
-            return
-
-        dfs(cnt + 1, sum - numbers[cnt])
-        dfs(cnt + 1, sum + numbers[cnt])
-
-    dfs(0, 0)
+    dfs(0, numbers, target, 0)
     return answer
 
 
